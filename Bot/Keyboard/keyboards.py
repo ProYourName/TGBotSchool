6d1b371372acd7fde7d522adf267098e7aeba3b9
VOZHD <proyourname2@gmail.com>
2024-10-19 17:53:18 +0300

    -Клавиатуры вынесены в отдельный файл

---
+++ Bot/Keyboard/keyboards.py
@@ -0,0 +1,20 @@
+import json
+from aiogram.types import InlineKeyboardButton
+import os
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+with open(os.path.abspath("schoollist.json"), 'r') as js:
+    json_data = json.load(js)
+
+Clusters = [
+    InlineKeyboardButton(text="Кластер 1", callback_data="cl1"),
+    InlineKeyboardButton(text="Кластер 2", callback_data="cl2"),
+    InlineKeyboardButton(text="Кластер 3", callback_data="cl3")
+]
+
+
+Keyboard_cluster1 = [
+    InlineKeyboardButton(text=json_data["cluster1"][0]["name"], callback_data="cl1_pf_1"),
+    InlineKeyboardButton(text=json_data["cluster1"][1]["name"], callback_data="cl1_pf_2"),
+    InlineKeyboardButton(text=json_data["cluster1"][2]["name"], callback_data="cl1_pf_3")
+]
--- Bot/index.py
+++ Bot/index.py
@@ -1,24 +1,48 @@
 import asyncio
 import sys
 import logging
-from aiogram import Bot, Dispatcher
+import json
+from Bot.Keyboard.keyboards import Keyboard_cluster1, Clusters
+from Keyboard import keyboards
+from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, FSInputFile
+from aiogram.filters.callback_data import CallbackQuery
+from aiogram.types import Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder, KeyboardBuilder
+
 from config import TOKEN
+
 
 
 dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(*Clusters)
+    builder.adjust(1)
+    await message.answer("Здравствуйте! Это бот тех. поддержки, выберите вашу площадку", reply_markup=builder.as_markup())
+
+
+
+@dp.callback_query(F.data == "cl1")
+async def my_callback_foo(callback: CallbackQuery, bot: Bot):
+    builder = InlineKeyboardBuilder()
+    builder.add(*Keyboard_cluster1)
+    await callback.message.answer(text="Вы выбрали Кластер 1, укажите, пожалуйста, площадку",reply_markup=builder.as_markup())
+
+
+
+@dp.message(Command('test'))
+async def cmd_start(message: Message):
     await message.answer("Привет")
 
 async def main():
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    # And the run events dispatching
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
