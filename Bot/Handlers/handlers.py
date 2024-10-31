086fd9336aef3dab9186cef71b6cbf9777f352ca
VOZHD <proyourname2@gmail.com>
2024-10-31 15:08:17 +0300

    Добавлена машина состояний, реализованы уведомления о заявках и их содержании

--- Bot/Handlers/handlers.py
+++ Bot/Handlers/handlers.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from imaplib import IMAP4
 import sqlite3
 from aiogram import Router,F,Bot
@@ -5,12 +6,17 @@ from aiogram.types import Message
 from aiogram.filters.callback_data import CallbackQuery, CallbackData
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.filters import Command,CommandStart, MagicData
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from Bot.Keyboard.keyboards import Clusters, ChoiceCluster, Keyboard_clusters, ChoiceStructure, json_data
 from Bot.Database.database import add_data_db
 
 router = Router()
 user_message = {}
-Flag = False
+
+class Form(StatesGroup):
+    choose_class = State()
+    write_message = State()
 
 
 @router.message(CommandStart())
@@ -28,20 +34,25 @@ async def cluster_callback(callback: CallbackQuery, callback_data: ChoiceCluster
     await callback.message.answer(text=f"Вы выбрали Кластер {callback_data.cluster_number}, укажите, пожалуйста, площадку",reply_markup=builder.as_markup())
 
 @router.callback_query(ChoiceStructure.filter())
-async def platform_callback(callback: CallbackQuery, callback_data: ChoiceStructure):
-    await callback.message.answer(text="Вы выбрали платформу, опишите Вашу проблему и отправьте сообщение боту")
+async def platform_callback(callback: CallbackQuery, callback_data: ChoiceStructure,state: FSMContext):
+    await callback.message.answer(text="Вы выбрали платформу, напишите номер аудитории")
+    await state.set_state(Form.choose_class)
     user_message.update(
         {"address": json_data[f"cluster{callback_data.cluster_number}"][callback_data.structure_number]["address"],
          "name": json_data[f"cluster{callback_data.cluster_number}"][callback_data.structure_number]["name"],}
     )
 
+@router.message(Form.choose_class)
+async def get_class(message: Message,state: FSMContext):
+    user_message.update({"class_number": message.text})
+    await message.answer("Теперь опишите вашу проблему")
+    await state.set_state(Form.write_message)
 
-@router.message()
-async def report(message: Message):
-    if len(user_message) == 2:
-        user_message.update({"message": message.text})
-        await message.answer("Ваше сообещение передано в тех. поддержку")
-        await add_data_db(user_message)
-        user_message.clear()
-    else:
-        await message.answer("Вы не выбрали адрес")
+@router.message(Form.write_message)
+async def report(message: Message,bot: Bot):
+    user_message.update({"message": message.text})
+    await message.answer("Ваше сообещение передано в тех. поддержку")
+    #await add_data_db(user_message)
+    await bot.send_message(chat_id="-4503968763",text=f"Получена новая заявка!\nАдрес - {user_message["address"]}\nНомер аудитории - {user_message["class_number"]}\nСообщение - {user_message["message"]}")
+    user_message.clear()
+
